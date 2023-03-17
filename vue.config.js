da7ba37333ec2d72d69aafb857110e009099b987
lhy_fairy <lhyxz1005@163.com>
2023-03-17 11:30:49 +0800

    尚品汇完成

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  productionSourceMap: false, //map文件体积大，可以删除（但不能定位错误，加密了）
+  transpileDependencies: true,
+  lintOnSave: false, //关闭语法检查
+  devServer: {
+    proxy: {
+      "/api": {
+        // 匹配所有以 '/api1'开头的请求路径
+        target: "http://gmall-h5-api.atguigu.cn", // 代理目标的基础路径
+        changeOrigin: true, //用于控制请求头中的host值  localhost5000   如果是false，实话实说 localhost8080
+        // pathRewrite: { "^/api": "" }, //匹配以api开头的，防止把路径api/带到服务器，服务器找不到
+      },
+    },
+  },
+});
